4ba664e8759e6bcd144cef42ccdcc742afe55960
bubxi <vcasoar@outlook.com>
2025-05-24 23:47:53 +1000

    Frontend foundation created

---
+++ frontend/client/src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ProductComponent } from './product/product.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'product', component: ProductComponent },
+  { path: '**', redirectTo: '' }
+];
